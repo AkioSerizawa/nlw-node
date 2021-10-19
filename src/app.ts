52c1a075808c7d494984c874014ec98e90b60b32
Akio <akio50@hotmail.com>
2021-10-19 00:26:24 -0300

    p1-au1

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,22 @@
-import express  from "express";
+import "dotenv/config"
+import express from "express";
 
+import { router } from "./routes";
+
+const app = express();
+app.use(express.json());
+
+app.use(router);
+
+app.get("/github", (request, response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+    );
+});
+
+app.get("/signin/callback", (request, response) => {
+    const { code } = request.query;
+
+    return response.json(code);
+})
+
+app.listen(4000, () => console.log(`O servidor está sendo executado na porta 4000`));
